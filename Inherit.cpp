9fbadfd9b24b86f8675f70920f8a48a0b96752d3
Hb-key <44340302+Hb-key@users.noreply.github.com>
2019-01-24 20:35:41 +0800

    Add files via upload

--- Inherit.cpp
+++ Inherit.cpp
@@ -1,21 +1,37 @@
-
 #include<iostream>
+using namespace std;
 
 class Base {
 public:
 	virtual void fun1()
-	{}
+	{
+		cout << "Base::fun1()" << endl;
+	}
 	virtual void fun2()
-	{}
-private:
-	int _b;
+	{
+		cout << "Base::fun2()" << endl;
+	}
 };
 
 class Derived :public Base {
 	virtual void fun1()
-	{}
+	{
+		cout << "Derived::fun1()" << endl;
+	}
 	virtual void fun3()
-	{}
-private:
-	int _d;
+	{
+		cout << "Derived::fun3()" << endl;
+	}
 };
+
+int main()
+{
+	Base b;
+	Derived d;
+	b.fun1();
+	b.fun2();
+	d.
+	d.fun1();
+	d.fun2();
+	return 0;
+}
