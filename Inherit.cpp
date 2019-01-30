ab80d7e7dd82df0e857aca0ef51a5a107a07ce8d
Hb-key <44340302+Hb-key@users.noreply.github.com>
2019-01-30 22:26:17 +0800

    Add files via upload

--- Inherit.cpp
+++ Inherit.cpp
@@ -1,28 +1,55 @@
+//#include<iostream>
+//using namespace std;
+//
+//class Base {
+//public:
+//	virtual void fun1()
+//	{
+//		cout << "Base::fun1()" << endl;
+//	}
+//};
+//
+//class Derived :public Base {
+//	virtual void fun1()
+//	{
+//		cout << "Derived::fun1()" << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	Base b, *p;
+//	Derived d;
+//	p = &b;
+//	p->fun1();
+//	p = &d;
+//	p->fun1();
+//	return 0;
+//}
+
 #include<iostream>
 using namespace std;
 
-class Base {
-public:
-	virtual void fun1()
-	{
-		cout << "Base::fun1()" << endl;
-	}
-};
+//class Base {
+//public:
+//	virtual ~Base()
+//	{
+//		cout << "~Base()" << endl;
+//	}
+//};
+//
+//class Derived :public Base {
+//public:
+//	~Derived ()
+//	{
+//		cout << "~Derived()" << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	Base* p1 = new Derived;
+//	delete p1;   
+//	return 0;
+//}
 
-class Derived :public Base {
-	virtual  void  fun1（）
-	{
-		cout << "Derived::fun1()" << endl;
-	}
-};
-
-int main()
-{
-	Base b, *p;
-	Derived d;
-	p = &b;
-	p->fun1();
-	p = &d;
-	p->fun1();
-	return 0;
-}
